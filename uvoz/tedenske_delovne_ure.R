e8d25f43b0bafa213202dc8988e3b0acab4ce893
Klara Penko <klara.penko@gmail.com>
2019-12-12 09:51:21 +0100

    uvoz datotek v mapo podatki in začetek urejanja tabele tedenskih_delovnih_ur

--- uvoz/tedenske_delovne_ure.R
+++ uvoz/tedenske_delovne_ure.R
@@ -2,4 +2,9 @@ library(readr)
 library(tidyr)
 library(dplyr)
 
-P <- read_tsv("podatki/tedenske_delovne_ure", col_names =c())
+URE <- read_csv("podatki/tedenske_delovne_ure(2009-2018).csv",na=" ",skip=1,
+                col_names=c("leta","država","spol","delovni čas", "status", "neki1","neki2","povprečno ur", "opombe"),
+                locale=locale(encoding="windows-1250")) %>%
+      select(-"spol",-"neki1",-"delovni čas",-"neki2",-"opombe",-"status")
+
+
