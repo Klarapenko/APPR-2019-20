6267b9db63340f522bc39af5e9d84192e75bf828
Klara Penko <klara.penko@gmail.com>
2019-12-19 09:54:39 +0100

    leta kot številke

--- uvoz/stevilo_zlorab.R
+++ uvoz/stevilo_zlorab.R
@@ -6,3 +6,5 @@ ZLORABE<- read_csv("podatki/št.spolnih_zlorab(2009-2018).csv",na=":",skip=1,
                 col_names=c("leto","država","spol","neki1", "enota", "vrednost(v procentih)","opombe"),
                 locale=locale(encoding="windows-1250")) %>%
   select(-"spol",-"neki1",-"enota",-"opombe")
+
+ZLORABE$leto <- parse_integer(ZLORABE$leto)
