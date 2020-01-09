ce5c8f0e1ead81e4d8664431fd007602e9f1ac61
Klara Penko <klara.penko@gmail.com>
2020-01-09 10:02:00 +0100

    uvoz tabele samomori

--- uvoz/stevilo_samomorov.R
+++ uvoz/stevilo_samomorov.R
@@ -7,3 +7,7 @@ SAMOMORI <- read_csv("podatki/steviloo_samomorovv.csv",na=":",skip=1,
                 col_names=c("spol","država","stevilo","neki1", "neki2", "neki3","leto","vrednost", "opombe"),
                 locale=locale(encoding="windows-1250")) %>%
   select(-"spol",-"neki1",-"stevilo",-"opombe",-"neki3",-"neki2")
+
+SAMOMORI$leto <- parse_integer(SAMOMORI$leto)
+
+samomori <- gather(SAMOMORI, -dejavnosti, key=leto, value = stevilo , na.rm = TRUE)
