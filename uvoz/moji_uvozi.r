1e0fd182b99077210769f1ed7653d249244854b3
Klara Penko <klara.penko@gmail.com>
2020-01-12 22:56:09 +0100

    pobrisan stari uvoz iz porojekt.rmd

--- uvoz/moji_uvozi.r
+++ uvoz/moji_uvozi.r
@@ -6,13 +6,18 @@ library(dplyr)
 
 #ŠTEVILO SAMOMOROV
 
-DOLGOVI<- read_csv("podatki/kolicina_dolgov.csv",na=":",skip=1,
-                   col_names=c("leto","država","neki1","neki2", "enota", "vrednost(v procentih)","opombe"),
-                   locale=locale(encoding="windows-1250")) %>%
-  select(-"neki1",-"neki2",-"enota",-"opombe")
+SAMOMORI <- read_csv("podatki/steviloo_samomorovv.csv",na=":",skip=1,
+                     col_names=c("spol","država","stevilo","neki1", "neki2", "neki3","leto","vrednost", "opombe"),
+                     locale=locale(encoding="windows-1250")) %>%
+  select(-"spol",-"neki1",-"stevilo",-"opombe",-"neki3",-"neki2")
 
-DOLGOVI$leto <- parse_integer(DOLGOVI$leto)
+SAMOMORI$leto <- parse_integer(SAMOMORI$leto)
 
+samomori <- gather(SAMOMORI, -država, key=leto, value =vrednost , na.rm = TRUE)
+
+samomori$leto <- parse_integer(samomori$leto)
+
+samomori.vsi<- samomori %>% group_by(leto) %>% summarise(vsota=sum(vrednost))
 
 
 
