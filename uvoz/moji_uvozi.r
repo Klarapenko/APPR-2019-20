9bc16b0b3c00d0d357e6baecd7696c1a038c9aea
Klara Penko <klara.penko@gmail.com>
2020-01-13 12:31:06 +0100

    urejeni samomori in graf g1

--- uvoz/moji_uvozi.r
+++ uvoz/moji_uvozi.r
@@ -11,13 +11,9 @@ SAMOMORI <- read_csv("podatki/steviloo_samomorovv.csv",na=":",skip=1,
                      locale=locale(encoding="windows-1250")) %>%
   select(-"spol",-"neki1",-"stevilo",-"opombe",-"neki3",-"neki2")
 
-SAMOMORI$leto <- parse_integer(SAMOMORI$leto)
 
-samomori <- gather(SAMOMORI, -država, key=leto, value =vrednost , na.rm = TRUE)
 
-samomori$leto <- parse_integer(samomori$leto)
-
-samomori.vsi<- samomori %>% group_by(leto) %>% summarise(vsota=sum(vrednost))
+samomori.vsi<- SAMOMORI %>% group_by(leto) %>% summarise(vsota=sum(vrednost, na.rm=TRUE))
 
 
 
@@ -30,7 +26,6 @@ URE <- read_csv("podatki/tedenske_delovne_ure.csv",na=":",skip=1,
   select(-"spol",-"neki1",-"delovni čas",-"opombe",-"status",-"neki2")
 
 
-URE$leto <- parse_integer(URE$leto)
 
 
 
@@ -45,7 +40,7 @@ ZLOCINI <- read_csv("podatki/stevilo_zlocinov.csv",na=":",skip=1,
   select(-"neki1",-"enota",-"opombe",-"neki2")
 
 
-ZLOCINI$leto <- parse_integer(ZLOCINI$leto)
+
 
 
 
@@ -54,12 +49,12 @@ ZLOCINI$leto <- parse_integer(ZLOCINI$leto)
 
 #ŠTEVILO ZLORAB
 
-ZLORABE<- read_csv("podatki/št.spolnih_zlorab.csv",na=":",skip=1,
+ZLORABE<- read_csv("podatki/st.spolnih_zlorab.csv",na=":",skip=1,
                    col_names=c("leto","država","spol","neki1", "enota", "vrednost(v procentih)","opombe"),
                    locale=locale(encoding="windows-1250")) %>%
   select(-"spol",-"neki1",-"enota",-"opombe")
 
-ZLORABE$leto <- parse_integer(ZLORABE$leto)
+
 
 
 
@@ -72,7 +67,6 @@ DOLGOVI<- read_csv("podatki/kolicina_dolgov.csv",na=":",skip=1,
                    locale=locale(encoding="windows-1250")) %>%
   select(-"neki1",-"neki2",-"enota",-"opombe")
 
-DOLGOVI$leto <- parse_integer(DOLGOVI$leto)
 
 
 
