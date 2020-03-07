81843ad752fa6851fbff366d52636cf5eff2ef63
Klara Penko <klara.penko@gmail.com>
2020-03-07 21:27:00 +0100

    dodan zemljevid

--- uvoz/moji_uvozi.r
+++ uvoz/moji_uvozi.r
@@ -7,14 +7,15 @@ library(dplyr)
 #ŠTEVILO SAMOMOROV
 
 SAMOMORI <- read_csv("podatki/steviloo_samomorovv.csv",na=":",skip=1,
-                     col_names=c("spol","država","stevilo","neki1", "neki2", "neki3","leto","vrednost", "opombe"),
+                     col_names=c("spol","drzava","stevilo","neki1", "neki2", "neki3","leto","vrednost", "opombe"),
                      locale=locale(encoding="windows-1250")) %>%
   select(-"spol",-"neki1",-"stevilo",-"opombe",-"neki3",-"neki2")
 
+SAMOMORI$drzava[SAMOMORI$drzava == 'Germany (until 1990 former territory of the FRG)'] <- 'Germany'
 
 
 samomori.vsi<- SAMOMORI %>% group_by(leto) %>% summarise(vsota=sum(vrednost, na.rm=TRUE))
-samomori.drzave <- SAMOMORI %>% group_by(država, leto) %>% summarise(vsota=sum(vrednost, na.rm = TRUE))
+samomori.drzave <- SAMOMORI %>% group_by(drzava, leto) %>% summarise(vsota=sum(vrednost, na.rm = TRUE))
 
 #=======================================================================================================================================
 #SAMOMORI V PROCENTIH(2011-2016)
@@ -27,12 +28,19 @@ P_SAMOMORI <- read_csv("podatki/samomorii.csv",skip =1, na = ":",
                        ) %>%
   select(-"stolpec1", - "stolpec2", - "stolpec3", -"opombe", -"stolpec4")
 
+P_SAMOMORI$drzava[P_SAMOMORI$drzava == 'Germany (until 1990 former territory of the FRG)'] <- 'Germany'
+
 #===============================================================================
 #STEVILO SAMOMOROV PO SPOLU
 S_SAMOMORI <- read_csv("podatki/st.samomorov_po_spolu.csv", na=":",skip=1,
-                       col_names=c("spol", "država", "neki1", "neki2", "starost","neki3", "leto", "vrednost","opombe"),
+                       col_names=c("spol", "drzava", "neki1", "neki2", "starost","neki3", "leto", "vrednost","opombe"),
                        locale = locale(encoding="windows-1250")) %>%
     select(-"neki1", -"neki2", -"neki3", -"starost", -"opombe")
+
+
+S_SAMOMORI$drzava[S_SAMOMORI$drzava == 'Germany (until 1990 former territory of the FRG)'] <- 'Germany'
+S_SAMOMORI$drzava[S_SAMOMORI$drzava == 'France (metropolitan)'] <- 'France'
+
 
 samomori.zenske.moski.po.letih <- S_SAMOMORI %>% group_by(spol, leto) %>% summarise(vsota=sum(vrednost, na.rm=TRUE))
 
@@ -46,16 +54,18 @@ URE <- read_csv("podatki/tedenske_delovne_ure.csv",na=":",skip=1,
                 locale=locale(encoding="windows-1250")) %>%
   select(-"spol",-"neki1",-"delovni čas",-"opombe",-"status",-"neki2")
 
+URE$drzava[URE$drzava == 'Germany (until 1990 former territory of the FRG)'] <- 'Germany'
+
+
 povprecje.ur.drzave <- URE %>% group_by(drzava) %>% summarise(povprecje=sum(ure)/(10))
 
+#=====Zemljevid evrope=====
 
+#data("World")
+#svet<- tm_shape(World) +tm_polygons(border.col = "black")
 
-data("World")
-svet<- tm_shape(World) +tm_polygons(border.col = "black")
-
-evropskedrzave <- World%>%filter(continent == "Europe")
-names(evropskedrzave)
-
+#evropskedrzave <- World%>%filter(continent == "Europe")
+#names(evropskedrzave)
 
 
 #=======================================================================================================================================
@@ -63,26 +73,29 @@ names(evropskedrzave)
 #ŠTEVILO ZLOČINOV
 
 ZLOCINI <- read_csv("podatki/stevilo_zlocinov.csv",na=":",skip=1,
-                    col_names=c("leto","država","neki1","neki2", "enota", "vrednost", "opombe"),
+                    col_names=c("leto","drzava","neki1","neki2", "enota", "vrednost", "opombe"),
                     locale=locale(encoding="windows-1250")) %>%
   select(-"neki1",-"enota",-"opombe",-"neki2")
 
-zlocini.po.drzavah <- ZLOCINI %>% group_by(država)
+
+ZLOCINI$drzava[ZLOCINI$drzava == 'Germany (until 1990 former territory of the FRG)'] <- 'Germany'
+
+
+zlocini.po.drzavah <- ZLOCINI %>% group_by(drzava)
+
 
 
 
 #=======================================================================================================================================
 
-
-
-
-
 #ŠTEVILO ZLORAB
 #vrednost v tabeli je v procentih
 ZLORABE<- read_csv("podatki/st.spolnih_zlorab.csv",na=":",skip=1,
-                   col_names=c("leto","država","spol","neki1", "enota", "vrednost","opombe"),
+                   col_names=c("leto","drzava","spol","neki1", "enota", "vrednost","opombe"),
                    locale=locale(encoding="windows-1250")) %>%
   select(-"spol",-"neki1",-"enota",-"opombe")
+
+ZLORABE$drzava[ZLORABE$drzava == 'Germany (until 1990 former territory of the FRG)'] <- 'Germany'
 
 
 
@@ -94,14 +107,15 @@ ZLORABE<- read_csv("podatki/st.spolnih_zlorab.csv",na=":",skip=1,
 #vrednost v tabeli je v procentih - koliko v procentih od vseh prebivalcev države ima dolgove 
 
 DOLGOVI<- read_csv("podatki/dolgovi.csv",na=":",skip=1,
-                   col_names=c("leto","država","neki1","neki2", "enota", "vrednost","opombe"),
+                   col_names=c("leto","drzava","neki1","neki2", "enota", "vrednost","opombe"),
                    locale=locale(encoding="windows-1250")) %>%
   select(-"neki1",-"neki2",-"enota",-"opombe")
 
+DOLGOVI$drzava[DOLGOVI$drzava == 'Germany (until 1990 former territory of the FRG)'] <- 'Germany'
 
-
+povprecje.zadolzenih.v.posamezni.drzavi <- DOLGOVI %>% group_by(drzava) %>% summarise(povprecje=sum(vrednost)/(11))
 #=======
-#to še nevem če bom, prI uvozu kratice držav..... nakoncu preglej!
+#to še nevem če bom, pri uvozu kratice držav..... nakoncu preglej!
 #STOPNJA LOCITEV (je razmerje med številom razvez med letom in povprečnim prebivalstvom v tem letu. Vrednost je izražena na 1000 oseb.)
 #LOCITVE <- read_csv("podatki/stevilo_locitev.csv",na=":",skip=1,
  #                   col_names=c("neki1","neki2","drzava","2006", "2007", "2008","2009","2010", "2011", "2012","2013", "2014","2015", "2016", "2017"),
--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -12,25 +12,62 @@
 #povprecja <- druzine %>% group_by(obcina) %>%
 #  summarise(povprecje=sum(velikost.druzine * stevilo.druzin) / sum(stevilo.druzin))
 
-
+#====================================================================================================
+#=======GRAFI========================================================================================
+#====================================================================================================
 g1 <- ggplot(data = samomori.vsi, aes(x=leto, y=vsota)) + geom_point() + 
   ggtitle("Število samomorov v celotni Evropi po letih") + 
   theme(panel.background=element_rect(fill="grey"))
 
 g2 <- ggplot(data=samomori.zenske.moski.po.letih, aes(x=leto, y=vsota , color=spol)) +
-  geom_line() + ggtitle("samomori v celotni Evropi, ločeni po spolu") 
+  geom_line() + ggtitle("samomori v celotni Evropi, ločeni po spolu") +
+  theme(panel.background=element_rect(fill="grey"))
 
-g3 <- ggplot(data=ZLOCINI, aes(x=leto, y=vrednost, color=država)) + 
+g3 <- ggplot(data=ZLOCINI, aes(x=leto, y=vrednost, color=drzava)) + 
   geom_line() + ggtitle("odstotek samomorov")
 
-#z1<- tm_shape(merge(zemljevid, povprecje.ur.drzave, by.x="", by.y="država" )) + tm_polygons("povprecje",title="Povprečje") + tm_layout(title="Povprečno število tedenskih ur posamezne države na leto")
+#g4 <- ggplot(data=povprecje.zadolzenih.v.posamezni.drzavi ,aes(x=povprecje) ) + geom_bar(mapping = NULL,
+#                  data = povprecje.zadolzenih.v.posamezni.drzavi,
+#                      stat = "count",
+#                    position = "stack",width = NULL,
+#                  na.rm = FALSE,
+#                  show.legend = NA,
+#                  inherit.aes = TRUE)
 
 
-#z2 <- tm_shape(merge(evropskedrzave, povprecje.ur.drzave, by.x="drzava", by.y="povprecje"))+
- # tm_polygons("povprecje",title="Povprecje") + tm_layout(title="Povprečno število tedenskih ur posamezne države na leto") 
-
-
-
-z1 <- tm_shape(evropskedrzave) + tm_polygons(border.col = "black") + tm_legend(show=FALSE)
+#z1 <- tm_shape(evropskedrzave) + tm_polygons(border.col = "black") + tm_legend(show=FALSE) 
 
 # + tmap_options(categories=ncol(names)
+
+
+
+#====================================================================================================
+#=======ZEMLJEVIDI===================================================================================
+#====================================================================================================
+
+data("World")
+svet<- tm_shape(World) +tm_polygons(border.col = "black")
+
+evropskedrzave <- World%>%filter(continent == "Europe")
+names(evropskedrzave)
+
+
+
+#kolikšen procent vseh smrti so predstavljali samomori v letu 2016:
+
+zemljevid.evrope <- function(){
+  evropa <- World %>% filter (continent == 'Europe')
+  Samomori <- P_SAMOMORI
+  Samomori <- P_SAMOMORI %>% filter (leto == 2016) %>% select('drzava', 'vrednost')
+  podatki <- merge(y = Samomori,x = evropa, by.x='name', by.y = 'drzava')
+  evropa <- tm_shape(podatki) + tm_polygons('vrednost') 
+  tmap_mode('view')
+  return(evropa)
+}
+zemljevid.evrope()
+
+
+
+
+
+
